3c57f481c1b00032bbecf1a8a73174e3fd884549
Poul Cummings <poul@cummings.dk>
2025-11-14 01:27:45 +0700

    wip

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactStrictMode: true,
-  output: 'export', // Enable static export
-  images: {
-    unoptimized: true, // Required for static export with Next.js Image component
-  },
 };
 
 export default nextConfig;
